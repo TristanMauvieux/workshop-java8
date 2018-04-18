1a1c7ac88172017018c1596db6c1cdee47afb5bb
Tristan Mauvieux <mauvieux.tristan@gmail.com>
2018-04-18 12:01:25 +0200

    function02 reussi

--- src/test/java/java8/ex02/Function_02_Test.java
+++ src/test/java/java8/ex02/Function_02_Test.java
@@ -17,14 +17,21 @@ public class Function_02_Test {
     //  tag::buildAccount[]
     // TODO Compléter la fonction buildAccount
     // TODO la fonction possède 2 paramètres en entrée : une personne et un solde
-    BiFunction<Person, Integer, Account> buildAccount = null;
+    BiFunction<Person, Integer, Account> buildAccount = (p,entier)->{
+    	Account compte = new Account();
+    	compte.setBalance(entier);
+    	compte.setOwner(p);
+    	return compte;
+    	
+    };
     //  end::buildAccount[]
 
     @Test
     public void test_build_account() throws Exception {
 
         // TODO invoquer la fonction buildAccount pour que le test soit passant
-        Account account = null;
+    	Person p = new Person("John", "France", 80, "pass");
+        Account account = buildAccount.apply(p, 500);
 
         assertThat(account, hasProperty("balance", is(500)));
         assertThat(account.getOwner(), hasProperty("firstname", is("John")));
