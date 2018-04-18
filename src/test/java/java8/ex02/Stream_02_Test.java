325ce94c597dd401af27e9bcb404e265fedd9874
Tristan Mauvieux <mauvieux.tristan@gmail.com>
2018-04-18 17:59:35 +0200

    stream 02 reussi

--- src/test/java/java8/ex02/Stream_02_Test.java
+++ src/test/java/java8/ex02/Stream_02_Test.java
@@ -1,17 +1,19 @@
 package java8.ex02;
 
-import java8.data.Data;
-import java8.data.domain.Customer;
-import java8.data.domain.Order;
-import java8.data.domain.Pizza;
-import org.junit.Test;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
 
 import java.util.IntSummaryStatistics;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import static org.hamcrest.Matchers.*;
-import static org.junit.Assert.*;
+import org.junit.Test;
+
+import java8.data.Data;
+import java8.data.domain.Customer;
+import java8.data.domain.Order;
+import java8.data.domain.Pizza;
 
 /**
  * Exercice 02 - Transformation
@@ -24,7 +26,7 @@ public class Stream_02_Test {
         List<Order> orders = new Data().getOrders();
 
         // Trouver la liste des clients ayant déjà passés une commande
-        List<Customer> result = null;
+        List<Customer> result = orders.stream().map(o -> o.getCustomer()).distinct().collect(Collectors.toList());
 
         assertThat(result, hasSize(2));
     }
@@ -36,7 +38,7 @@ public class Stream_02_Test {
 
         // TODO calculer les statistiques sur les prix des pizzas vendues
         // TODO utiliser l'opération summaryStatistics
-        IntSummaryStatistics result = null;
+        IntSummaryStatistics result = orders.stream().flatMap(o -> o.getPizzas().stream()).collect(Collectors.summarizingInt(Pizza::getPrice));
 
 
         assertThat(result.getSum(), is(10900L));
